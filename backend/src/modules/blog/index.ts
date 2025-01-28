c4f035107fabb8c7415568a0e4670498131e96a2
Yagnesh Modh <contact@thespecialcharacter.com>
2025-01-28 10:38:33 +0530

    fix changes

---
+++ backend/src/modules/blog/index.ts
@@ -0,0 +1,8 @@
+import BlogModuleService from "./service";
+import { Module } from "@medusajs/framework/utils";
+
+export const BLOG_MODULE = "blogModuleService";
+
+export default Module(BLOG_MODULE, {
+	service: BlogModuleService,
+});
---
+++ backend/src/modules/blog/service.ts
@@ -0,0 +1,10 @@
+import { MedusaService } from "@medusajs/framework/utils";
+import Blog from "./models/blog";
+import BlogSeo from "./models/blog_seo";
+
+class BlogModuleService extends MedusaService({
+	Blog,
+	BlogSeo,
+}) {}
+
+export default BlogModuleService;
