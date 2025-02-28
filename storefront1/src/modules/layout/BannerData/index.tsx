8e15b75fa670196e903f8894fedf0c93eec20274
akshpatell <akshpatel08032003@gmail.com>
2025-02-28 17:20:07 +0530

    add swiper in bannerData component

--- storefront1/src/modules/layout/BannerData/index.tsx
+++ storefront1/src/modules/layout/BannerData/index.tsx
@@ -1,7 +1,19 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
 import { BannerProps } from "types/global"
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+// import "./styles.css"
+
+import { Autoplay } from "swiper/modules"
 
 type Props = {
   data: { bannerList: BannerProps[] }
@@ -12,55 +24,67 @@ export const BannerData = ({ data }: Props) => {
 
   return (
     <>
-      {bannerData.map((item) => {
-        return (
-          <div className="bg-dolginsblue">
-            {item.link ? (
-              <Link href={item.link}>
-                <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
-                  <div
-                    key={item.id}
-                    className="flex items-center gap-4 text-sm font-medium text-white hover:text-gray-100"
-                  >
-                    {item.image && (
-                      <div className="relative h-4 aspect-square">
-                        <Image
-                          alt={item.name}
-                          src={item.image || ""}
-                          className=""
-                          fill
-                        />
-                      </div>
-                    )}
-                    <p>{item.text}</p>
-                  </div>
-                </div>
-              </Link>
-            ) : (
+      <Swiper
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+        }}
+        loop={true}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        {bannerData.map((item) => {
+          return (
+            <SwiperSlide>
               <div className="bg-dolginsblue">
-                <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
-                  <div
-                    key={item.id}
-                    className="flex items-center space-x-6 text-sm font-medium text-white hover:text-gray-100"
-                  >
-                    {item.image && (
-                      <div className="relative">
-                        <Image
-                          alt={item.name}
-                          src={item.image || ""}
-                          className=""
-                          fill
-                        />
+                {item.link ? (
+                  <Link href={item.link}>
+                    <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
+                      <div
+                        key={item.id}
+                        className="flex items-center gap-4 text-sm font-medium text-white hover:text-gray-100"
+                      >
+                        {item.image && (
+                          <div className="relative h-4 aspect-square">
+                            <Image
+                              alt={item.name}
+                              src={item.image || ""}
+                              className=""
+                              fill
+                            />
+                          </div>
+                        )}
+                        <p>{item.text}</p>
                       </div>
-                    )}
-                    <p>{item.text}</p>
+                    </div>
+                  </Link>
+                ) : (
+                  <div className="bg-dolginsblue">
+                    <div className="mx-auto flex h-10 max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8">
+                      <div
+                        key={item.id}
+                        className="flex items-center space-x-6 text-sm font-medium text-white hover:text-gray-100"
+                      >
+                        {item.image && (
+                          <div className="relative">
+                            <Image
+                              alt={item.name}
+                              src={item.image || ""}
+                              className=""
+                              fill
+                            />
+                          </div>
+                        )}
+                        <p>{item.text}</p>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                )}
               </div>
-            )}
-          </div>
-        )
-      })}
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
     </>
   )
 }
